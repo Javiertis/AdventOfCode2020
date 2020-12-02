8835bc9f82f5b22b2b1459c839b63adebe9b4b9a
Javiertis <javi.teje99@gmail.com>
2020-12-02 16:35:35 +0100

    dia 1 y 2

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -1,25 +1,66 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
-    class Day01 : ASolution
+    internal class Day01 : ASolution
     {
+        private readonly List<int> _input;
 
         public Day01() : base(01, 2020, "")
         {
-
+            _input = Input.SplitByNewline().AsEnumerable().Select(int.Parse).ToList();
         }
 
         protected override string SolvePartOne()
         {
+            return NestedForeach();
+        }
+
+        private string NestedForeach()
+        {
+            var memo = new HashSet<int>();
+            foreach (var input in _input)
+            {
+                if (input > 2020) continue;
+                foreach (var n in memo)
+                {
+                    if (n + input == 2020)
+                    {
+                        return (n * input).ToString();
+                    }
+                }
+                memo.Add(input);
+            }
+            return null;
+        }
+
+        private string NestedClassicFors()
+        {
+            for (int i = 0; i < _input.Count; i++)
+            {
+                for (int j = i + 1; j < _input.Count; j++)
+                {
+                    if (_input[i] + _input[j] == 2020) return $"{_input[i] * _input[j]}";
+                }
+            }
             return null;
         }
 
         protected override string SolvePartTwo()
         {
+            for (int i = 0; i < _input.Count; i++)
+            {
+                for (int j = i + 1; j < _input.Count; j++)
+                {
+                    for (int k = j + 1; k < _input.Count; k++)
+                    {
+                        if (_input[i] + _input[j] + _input[k] == 2020) return $"{_input[i] * _input[j] * _input[k]}";
+                    }
+                }
+            }
             return null;
         }
     }
--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -1,26 +1,50 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
-    class Day02 : ASolution
+    internal class Day02 : ASolution
     {
+        private string[] _input;
+        private int validPassCount, min, max, letterCount;
+        private string letter, pass;
 
         public Day02() : base(02, 2020, "")
         {
-
+            _input = Input.SplitByNewline();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            validPassCount = 0;
+            foreach (var item in _input)
+            {
+                string[] tmp = item.Split(new char[] { ':', '-', ' ' });
+                min = int.Parse(tmp[0]);
+                max = int.Parse(tmp[1]);
+                letter = tmp[2];
+                pass = tmp[4];
+                letterCount = pass.Count(a => letter.Contains(a));
+                if (letterCount >= min && letterCount <= max) validPassCount++;
+            }
+            return validPassCount.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            validPassCount = 0;
+            foreach (var item in _input)
+            {
+                string[] tmp = item.Split(new char[] { ':', '-', ' ' });
+                min = int.Parse(tmp[0]);
+                max = int.Parse(tmp[1]);
+                letter = tmp[2];
+                pass = tmp[4];
+                if (pass[min - 1].ToString() == letter ^ pass[max - 1].ToString() == letter) validPassCount++;
+            }
+            return validPassCount.ToString();
         }
     }
 }
