b6265ffdd9d5a42ffb4be8385308bf5054988ad4
Javiertis <javi.teje99@gmail.com>
2020-12-06 12:18:05 +0100

    dia 6

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -1,26 +1,72 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
-    class Day06 : ASolution
+    internal class Day06 : ASolution
     {
+        private List<string> _input;
 
         public Day06() : base(06, 2020, "")
         {
-
+            _input = ParseInput();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return _input
+                   .Select(
+                            a => a.Where(a => a != '\n')
+                                  .Distinct()
+                                  .Count()
+                          )
+                   .Sum()
+                   .ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int total = 0;
+            int[] counter;
+            string[] groupLines;
+            foreach (var group in _input)
+            {
+                counter = new int[26];
+                groupLines = group.Split('\n');
+                groupLines.JoinAsStrings()
+                          .ToList()
+                          .ForEach(a => counter[a - 'a']++);
+                total += counter.Count(a => a == groupLines.Length);
+            }
+            return total.ToString();
+        }
+
+        private List<string> ParseInput()
+        {
+            List<string> groups = new List<string>();
+
+            int index = 0;
+
+            foreach (string line in Input.Split("\n", StringSplitOptions.None))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    ++index;
+                    continue;
+                }
+                else if (groups.Count <= index)
+                {
+                    groups.Add(line);
+                }
+                else
+                {
+                    groups[index] += line + '\n';
+                }
+            }
+
+            return groups;
         }
     }
 }
